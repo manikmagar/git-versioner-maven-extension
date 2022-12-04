af40b6c4304ac51309fa520a3b2e12c5ee96be7e
Manik Magar <manik.magar@gmail.com>
2022-12-04 16:14:55 -0500

    feat: add tag goal (#4)

--- src/main/java/com/github/manikmagar/maven/versioner/git/JGitVersioner.java
+++ src/main/java/com/github/manikmagar/maven/versioner/git/JGitVersioner.java
@@ -2,17 +2,15 @@
 package com.github.manikmagar.maven.versioner.git;
 
 import com.github.manikmagar.maven.versioner.Version;
-import com.github.manikmagar.maven.versioner.mojo.params.VersionConfig;
 import com.github.manikmagar.maven.versioner.Versioner;
+import com.github.manikmagar.maven.versioner.mojo.params.VersionConfig;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.RevCommit;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class JGitVersioner implements Versioner {
 
@@ -23,35 +21,31 @@ public class JGitVersioner implements Versioner {
 	}
 
 	public Version version() {
-		try (Repository repository = new FileRepositoryBuilder().readEnvironment().findGitDir().build()) {
-			try (Git git = new Git(repository)) {
-				var branch = git.getRepository().getBranch();
-				Ref head = git.getRepository().findRef("HEAD");
-				var hash = "";
-				if (head != null && head.getObjectId() != null) {
-					hash = head.getObjectId().getName();
-				}
-				var version = new Version(branch, hash, versionConfig.getInitial().getMajor(),
-						versionConfig.getInitial().getMinor(), versionConfig.getInitial().getPatch());
-				var commits = git.log().call();
-				List<RevCommit> revCommits = StreamSupport.stream(commits.spliterator(), false)
-						.collect(Collectors.toList());
-				Collections.reverse(revCommits);
-				for (RevCommit commit : revCommits) {
-					if (commit.getFullMessage().contains(versionConfig.getKeywords().getMajorKey())) {
-						version.incrementMajor();
-					} else if (commit.getFullMessage().contains(versionConfig.getKeywords().getMinorKey())) {
-						version.incrementMinor();
-					} else if (commit.getFullMessage().contains(versionConfig.getKeywords().getPatchKey())) {
-						version.incrementPatch();
-					} else {
-						version.incrementCommit();
-					}
-				}
-				return version;
+		return JGit.executeOperation(git -> {
+			var branch = git.getRepository().getBranch();
+			Ref head = git.getRepository().findRef("HEAD");
+			var hash = "";
+			if (head != null && head.getObjectId() != null) {
+				hash = head.getObjectId().getName();
 			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+			var version = new Version(branch, hash, versionConfig.getInitial().getMajor(),
+					versionConfig.getInitial().getMinor(), versionConfig.getInitial().getPatch());
+			var commits = git.log().call();
+			List<RevCommit> revCommits = StreamSupport.stream(commits.spliterator(), false)
+					.collect(Collectors.toList());
+			Collections.reverse(revCommits);
+			for (RevCommit commit : revCommits) {
+				if (commit.getFullMessage().contains(versionConfig.getKeywords().getMajorKey())) {
+					version.incrementMajor();
+				} else if (commit.getFullMessage().contains(versionConfig.getKeywords().getMinorKey())) {
+					version.incrementMinor();
+				} else if (commit.getFullMessage().contains(versionConfig.getKeywords().getPatchKey())) {
+					version.incrementPatch();
+				} else {
+					version.incrementCommit();
+				}
+			}
+			return version;
+		});
 	}
 }
